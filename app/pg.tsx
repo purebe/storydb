c06afe7b7e6599ab4ed43644efe5568361a02fef
Purebe <curebe@gmail.com>
2024-01-10 05:44:04 +0000

    Reuse same db pool for auth adapter

--- app/pg.tsx
+++ app/pg.tsx
@@ -1,18 +1,19 @@
 import { Pool } from 'pg';
 
-let pool;
+const pool = new Pool({
+  host: process.env.PG_HOST as string,
+  user: process.env.PG_USER as string,
+  port: process.env.PG_PORT as number,
+  password: process.env.PG_PASSWORD as string,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMilllis: 2000
+});
 
 export function PgConnect() {
-  if (pool) return pool.connect();
-
-  pool = new Pool({
-          host: process.env.PG_HOST as string,
-          user: process.env.PG_USER as string,
-          port: process.env.PG_PORT as number,
-          password: process.env.PG_PASSWORD as string,
-          max: 20,
-          idleTimeoutMillis: 30000,
-          connectionTimeoutMilllis: 2000
-  });
   return pool.connect();
+}
+
+export function GetSharedPool() {
+  return pool;
 }
