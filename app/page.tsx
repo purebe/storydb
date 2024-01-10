d29efc6e17824f697badf84565488abf023b54ee
Purebe <curebe@gmail.com>
2024-01-10 06:12:35 +0000

    fixed main project page layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,21 @@ import { redirect } from 'next/navigation';
 import { Project } from '@/app/project';
 
 export default async function Home() {
-	const session = await getServerSession(authOptions);
-
-	if (!session) {
-		redirect('/login', 'push');
-	}
-	
-	return (
-		<div className="h-screen flex justify-center items-center bg-gray-900 bg-gradient-to-b from-gray-950 to-gray-900">
-			<div className="self-start">
-				<Project />
-			</div>
-			<div className="text-sm self-end mb-4">
-				<span>Signed in as {session.user && session.user.email} - </span>
-				<a href="/api/auth/signout" className="underline">Sign out</a>
-			</div>
-		</div>
-	);
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect('/login', 'push');
+  }
+    return (
+      <div className="h-screen flex flex-col justify-center text-center bg-gray-900 bg-gradient-to-b from-gray-950 to-gray-900">
+        <div className="self-start h-3/5 px-4">
+          <Project />
+        </div>
+        <div className="flex flex-col grow justify-end text-sm mb-4">
+          <div>
+            <span>Signed in as {session.user && session.user.email} - </span>
+            <a href="/api/auth/signout" className="underline">Sign out</a>
+          </div>
+        </div>
+      </div>
+    );
 }
--- app/project.tsx
+++ app/project.tsx
@@ -10,7 +10,7 @@ export async function Project() {
     markup = (
       <>
         {res.rows.map(row => (
-          <div className="bg-slate-100 w-48 h-52 rounded shadow-xl mt-8 pb-2 flex flex-col justify-around text-center font-light text-slate-900 text-base drop-shadow-xl cursor-pointer">
+          <div className="bg-slate-100 w-48 h-52 rounded shadow-xl mt-8 pb-2 flex flex-col justify-around text-center font-light text-slate-900 text-base drop-shadow-xl cursor-pointer hover:outline hover:outline-2 hover:outline-orange-300">
             <p className="font-bold drop-shadow-xl">{row.name}</p>
             <p className="text-sm w-48 h-32 text-ellipsis overflow-hidden">{row.description}</p>
             <span className="text-xs font-medium">Last updated at {row.updated_at.toLocaleDateString()}</span>
