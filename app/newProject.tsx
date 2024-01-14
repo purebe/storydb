a7a50a60410bcc2c332b1e14e7a0640c7a0ef235
Purebe <curebe@gmail.com>
2024-01-14 08:45:45 +0000

    redesigned new project a little

--- app/newProject.tsx
+++ app/newProject.tsx
@@ -2,8 +2,8 @@
 
 export function NewProject() {
   return (
-    <div className="w-48 h-52 rounded mt-8 pb-2 flex text-center items-center justify-center text-slate-100 text-6xl font-light drop-shadow-xl">
-      <a href='/project/-1'><span className="cursor-pointer">+</span></a>
+    <div className="w-12 h-52 rounded mt-8 pb-2 flex text-center items-center justify-center text-slate-300 text-6xl font-light drop-shadow-xl">
+      <a href='/project/-1'><span className="cursor-pointer hover:text-slate-50">+</span></a>
     </div>
   );
 }
